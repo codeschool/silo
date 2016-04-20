79a567b058af23e01b82eccde6fe382d5f9a88ec
Morgan Laco <morgan@codeschool.com>
2016-04-19 20:34:27 -0400

    Remove unused components

--- src/components/answer-component.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Answer extends React.Component {
-  render() {
-    return <div></div>;
-  }
-}
-
-export default Answer;
--- src/components/preview-component.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Preview extends React.Component {
-  render() {
-    return <div></div>;
-  }
-}
-
-export default Preview;
